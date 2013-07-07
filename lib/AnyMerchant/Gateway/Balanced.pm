72d408432c6bf894267e49a1ff1f3938168450ef
Naveed Massjouni <naveedm9@gmail.com>
2013-07-07 10:33:33 -0400

    Added support for capturing holds

--- lib/AnyMerchant/Gateway/Balanced.pm
+++ lib/AnyMerchant/Gateway/Balanced.pm
@@ -2,16 +2,38 @@ package AnyMerchant::Gateway::Balanced;
 use Moo;
 with 'AnyMerchant::Gateway';
 
+use Business::BalancedPayments;
+
+has '+base_url' => ( default => sub { 'https://api.balancedpayments.com' } );
+
+has api_keys_uri     => (is => 'ro', default => sub { '/v1/api_keys'     });
+has merchants_uri    => (is => 'ro', default => sub { '/v1/merchants'    });
+has marketplaces_uri => (is => 'ro', default => sub { '/v1/marketplaces' });
+
+has marketplace => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->get($self->marketplaces_uri)->{items}[0];
+    },
+);
+
+
 sub authorize {
+    my ($self) = @_;
 }
 
 sub capture {
+    my ($self, $hold_uri, %params) = @_;
+    my $data = { hold_uri => $hold_uri, %params };
+    return $self->post($self->marketplace->{debits_uri}, $data);
 }
 
 sub charge {
 }
 
-sub refund {
+sub credit {
 }
 
 sub void {
